62beded44874cb5b3c152be2e2926ebafc38ff72
bangnd <bangnd@abilive.vn>
2023-04-02 21:09:54 +0700

    draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Container from './Container';
+import Contact from './component/contact/Contact';
+import NotFound from './NotFound';
+import Slider from './component/silder/Slider';
+
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Container />}/>
+          <Route path="contact" element={<Contact />} />
+          <Route element={<NotFound />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
